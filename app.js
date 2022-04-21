ea3c39a21bc10208288906fa272a0ef2021e25c5
Robyn Alex <robyn.alexander1111@gmail.com>
2022-04-20 22:20:51 -0500

    first comm1t

--- app.js
+++ app.js
@@ -5,4 +5,12 @@ console.log("EXERCISE 1:\n==========\n");
 
 const http = require("http");
 
+http.createServer(function (request, response) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.end("Hello World");
+})
+.listen(3000, function () {
+    console.log("Server listening...");
+});
+
 // Finish setting up the server
